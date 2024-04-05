749d31c755358cb08209c2d23f38e1aa1c6fd96f
artog1809 <gdoq17@mail.ru>
2024-04-06 00:39:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".go-map").addEventListener('click', _ =>{
+    window.location.href = "https://yandex.ru/maps/-/CDRkI2KV"
+})
+
+document.querySelector(".fill-form").addEventListener('click', _ => {
+    window.location.href = "https://forms.gle/2UpexVPE1q32pMMS7"
+})
