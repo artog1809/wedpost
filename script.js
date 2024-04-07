6e8aad06a36eefc17e7e12995899bd84c79c217f
artog1809 <gdoq17@mail.ru>
2024-04-07 17:41:01 +0300

    fixed google form url

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ document.querySelector(".go-map").addEventListener('click', _ =>{
 })
 
 document.querySelector(".fill-form").addEventListener('click', _ => {
-    window.location.href = "https://forms.gle/2UpexVPE1q32pMMS7"
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSe-JEFbqbM4O5sTEl8QUvmhr2eyIBTLUV_kw0PhclZFlA7GTg/viewform?usp=sf_link"
 })
